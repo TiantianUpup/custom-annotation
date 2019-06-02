5cbdf004dafe22cb825512863635d0c62939fdb2
dayday <969795191@qq.com>
2019-06-02 09:02:45 +0800

    添加两个子模块

---
+++ custom-annotation-demo3/src/main/java/com/cqupt/annotation/CustomAnnotationDemo3Application.java
@@ -0,0 +1,13 @@
+package com.cqupt.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationDemo3Application.class, args);
+	}
+
+}
